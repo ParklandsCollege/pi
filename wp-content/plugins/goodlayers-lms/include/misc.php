c557ebb7f04c3ec5526da905d031b2cb7596b153
Neil Berrow <neil.berrow@hetzner.co.za>
2015-04-28 20:27:39 +0200

    Take out login stuff

--- wp-content/plugins/goodlayers-lms/include/misc.php
+++ wp-content/plugins/goodlayers-lms/include/misc.php
@@ -1,8 +1,8 @@
 <?php
-	/*	
+	/*
 	*	Goodlayers Misc File
 	*/
-	
+
 	function gdlr_lms_header_signin(){
 		if( is_user_logged_in() ){
 			global $current_user;
@@ -16,11 +16,11 @@
 			echo '<div class="gdlr-lms-header-signin">';
 			echo '<i class="icon-lock"></i>';
 			echo '<a data-rel="gdlr-lms-lightbox" data-lb-open="login-form" >' . __('Sign In', 'gdlr-lms') . '</a>';
-			gdlr_lms_sign_in_lightbox_form();
+			//gdlr_lms_sign_in_lightbox_form();
 			echo '<span class="gdlr-separator">|</span>';
 			echo '<a href="' . add_query_arg('register', get_permalink(), home_url()) . '">' . __('Sign Up', 'gdlr-lms') . '</a>';
-			echo '</div>';		
+			echo '</div>';
 		}
 	}
-	
+
 ?>
