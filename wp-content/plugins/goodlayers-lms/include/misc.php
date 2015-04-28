511908fdee38957a6182d92a3f99540d562e7197
Neil Berrow <neil.berrow@hetzner.co.za>
2015-04-28 20:53:28 +0200

    Menu mods

--- wp-content/plugins/goodlayers-lms/include/misc.php
+++ wp-content/plugins/goodlayers-lms/include/misc.php
@@ -15,7 +15,7 @@
 		}else{
 			echo '<div class="gdlr-lms-header-signin">';
 			echo '<i class="icon-lock"></i>';
-			echo '<a data-rel="gdlr-lms-lightbox" data-lb-open="login-form" >' . __('Sign In', 'gdlr-lms') . '</a>';
+			echo '<a data-rel="gdlr-lms-lightbox" data-lb-open="login-form" >/wp-admin</a>';
 			//gdlr_lms_sign_in_lightbox_form();
 			echo '<span class="gdlr-separator">|</span>';
 			echo '<a href="' . add_query_arg('register', get_permalink(), home_url()) . '">' . __('Sign Up', 'gdlr-lms') . '</a>';
